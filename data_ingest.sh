7e8703cf5ab0571a0c203da9f2e905a3d112a404
DANRAMAGE <ChiefDan@gmail.com>
2023-11-15 11:38:57 -0500

    Initial Checkin.

---
+++ data_ingest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /usr/local/virtualenv/pyenv-3.8.5/bin/activate
+
+cd /home/xeniaprod/scripts/DataIngestion
+
+python main.py --ConfigFile=/home/xeniaprod/scripts/DataIngestion/data_ingest.ini
